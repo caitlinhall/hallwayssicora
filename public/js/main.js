3d5b0c2ccdc405f01f7117dfb2a5f3ae56a5626f
Caitlin Hall <caitlin.hall2@target.com>
2015-08-22 21:57:45 -0500

    first commit

--- public/js/main.js
+++ public/js/main.js
@@ -1,26 +1,26 @@
-$(document).ready(function(){
-	$.ajax({
-		url: "/instagram",
-	}).done(function(data) {
-		var parsed = JSON.parse(data);
-		var picContainer = $("#picContainer");
-		if(picContainer){
-			for(var i = 0; i < parsed.length; i++){
-		        picContainer.append("<img id='grid' src='" + parsed[i] + "'alt='pic' />");
-		    }
-		}
-	});
+// $(document).ready(function(){
+// 	$.ajax({
+// 		url: "/instagram",
+// 	}).done(function(data) {
+// 		var parsed = JSON.parse(data);
+// 		var picContainer = $("#picContainer");
+// 		if(picContainer){
+// 			for(var i = 0; i < parsed.length; i++){
+// 		        picContainer.append("<img id='grid' src='" + parsed[i] + "'alt='pic' />");
+// 		    }
+// 		}
+// 	});
 	
-	$('a[href*=#]:not([href=#])').click(function() {
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-		  var target = $(this.hash);
-		  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-		  if (target.length) {
-		    $('html,body').animate({
-		      scrollTop: target.offset().top
-		    }, 1000);
-		    return false;
-		  }
-		}
-	});
-});
+// 	$('a[href*=#]:not([href=#])').click(function() {
+// 		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+// 		  var target = $(this.hash);
+// 		  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+// 		  if (target.length) {
+// 		    $('html,body').animate({
+// 		      scrollTop: target.offset().top
+// 		    }, 1000);
+// 		    return false;
+// 		  }
+// 		}
+// 	});
+// });
--- web.js
+++ web.js
@@ -2,39 +2,10 @@ require('newrelic');
 var express = require('express');
 var app = express();
 var path = require('path');
-var api = require('instagram-node').instagram();
 
 app.use(express.logger());
 
 app.use(express.static(path.join(__dirname, 'public')));
-
-api.use({
-  client_id: process.env.INSTAGRAM_CLIENT_ID,
-  client_secret: process.env.INSTAGRAM_CLIENT_SECRET
-});
-
-var userId = "1626659574";
-
-// endpoint
-app.get('/instagram', function (req, res, next) {
-  var count = req.query.count || 8;
-  // example: call user_media_recent to get lastest user posts
-  api.user_media_recent(userId, { count: count }, function (err, medias, pagination, remaining, limit) {
-    if (err) {
-      next(err);
-    } else {
-      var i;
-      var arrayLength = medias.length;
-      var instagramImagesArray = [];
-      for(i = 0; i < arrayLength; i++){
-        //.push adds a new string url to the array. it allows the ability to individually add data.
-        instagramImagesArray.push(medias[i].images.standard_resolution.url);
-      }
-      //transforms the data to being in json! with the help of JSON.stringify
-      res.send(JSON.stringify(instagramImagesArray));
-    }
-  });
-});
 
 var port = process.env.PORT || 5000;
 
