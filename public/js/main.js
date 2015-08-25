1f3b36f5252857cbd63c71c80d8932cbeb8ef84f
Caitlin Hall <caitlin.hall2@target.com>
2015-08-24 22:32:38 -0500

    timeline

--- public/js/main.js
+++ public/js/main.js
@@ -1,26 +1,27 @@
-// $(document).ready(function(){
-// 	$.ajax({
-// 		url: "/instagram",
-// 	}).done(function(data) {
-// 		var parsed = JSON.parse(data);
-// 		var picContainer = $("#picContainer");
-// 		if(picContainer){
-// 			for(var i = 0; i < parsed.length; i++){
-// 		        picContainer.append("<img id='grid' src='" + parsed[i] + "'alt='pic' />");
-// 		    }
-// 		}
-// 	});
-	
-// 	$('a[href*=#]:not([href=#])').click(function() {
-// 		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-// 		  var target = $(this.hash);
-// 		  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-// 		  if (target.length) {
-// 		    $('html,body').animate({
-// 		      scrollTop: target.offset().top
-// 		    }, 1000);
-// 		    return false;
-// 		  }
-// 		}
-// 	});
-// });
+//timeline functions
+$(document).ready(function($){
+	var timelineBlocks = $('.cd-timeline-block'),
+		offset = 0.8;
+
+	//hide timeline blocks which are outside the viewport
+	hideBlocks(timelineBlocks, offset);
+
+	//on scolling, show/animate timeline blocks when enter the viewport
+	$(window).on('scroll', function(){
+		(!window.requestAnimationFrame) 
+			? setTimeout(function(){ showBlocks(timelineBlocks, offset); }, 100)
+			: window.requestAnimationFrame(function(){ showBlocks(timelineBlocks, offset); });
+	});
+
+	function hideBlocks(blocks, offset) {
+		blocks.each(function(){
+			( $(this).offset().top > $(window).scrollTop()+$(window).height()*offset ) && $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
+		});
+	}
+
+	function showBlocks(blocks, offset) {
+		blocks.each(function(){
+			( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+		});
+	}
+});
