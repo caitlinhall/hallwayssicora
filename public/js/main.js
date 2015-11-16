cd8d30ce4cd8947f6a96a5231d69f29e2b8d8f9d
Caitlin Hall <caitlin.hall2@target.com>
2015-11-15 20:15:27 -0600

    mobile nav please work

--- public/js/main.js
+++ public/js/main.js
@@ -25,4 +25,22 @@ $(document).ready(function($){
 		});
 	}
 
+	$('a[href*=#]:not([href=#])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+		  var target = $(this.hash);
+		  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		  if (target.length) {
+		    $('html,body').animate({
+		      scrollTop: target.offset().top
+		    }, 1000);
+		    $(".new-header").toggle("slow");
+		    return false;
+		  }
+		}
+	});
+
+	$("#menu").click(function() {
+  		$(".new-header").toggle("slow");
+  	});
+
 });
